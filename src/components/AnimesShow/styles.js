246813eefbbb00c4f88c08c95ba57582a0221255
zBrownie <bru.nomoraes@hotmail.com>
2020-03-10 22:30:04 -0300

    css grid

--- src/components/AnimesShow/styles.js
+++ src/components/AnimesShow/styles.js
@@ -1,92 +1,83 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Container = styled.div `
+export const Container = styled.div`
+  flex: 1;
 
- 
- flex:1;
-
-  h1{
-    text-align:center;
+  h1 {
+    text-align: center;
     margin-top: 40px;
   }
 
-  p{
-    text-align:center;
+  p {
+    text-align: center;
     margin: 10px 0;
-    font-size:18px;
-    font-weight:bold;
+    font-size: 18px;
+    font-weight: bold;
   }
 
- ul{
-    display:grid;
-      grid-template-columns: 1fr 1fr 1fr 1fr;
-      align-items:center;
-      margin:10px auto;
-      
- }
-
- 
- li{
-      list-style:none;
-      
+  ul {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+    align-items: center;
+    margin: 10px auto;
   }
 
-  @media (max-width:850px){
-    ul{
+  li {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+  }
+
+  @media (max-width: 850px) {
+    ul {
       grid-template-columns: 1fr 1fr 1fr;
-  }
+    }
   }
 
-  @media (max-width:700px){
-    ul{
+  @media (max-width: 700px) {
+    ul {
       grid-template-columns: 1fr 1fr;
-  }
+    }
   }
 
-  @media (max-width:480px){
-    ul{
+  @media (max-width: 480px) {
+    ul {
       grid-template-columns: 1fr;
+    }
   }
-}
 `;
 
-export const PosterAnime = styled.div `
-    margin: 12px auto;
-    width:200px;
-    height:300px;
-    background-image: url(${props => props.src});
-    background-repeat:no-repeat;
-    background-size:cover;
-    border-radius:3px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
+export const PosterAnime = styled.div`
+  margin: 12px auto;
+  width: 200px;
+  height: 300px;
+  background-image: url(${props => props.src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 3px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 
-    transition:1s;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
-    :hover{
-      transform:scale(1.09);
-    }
+  transition: 1s;
 
-    span{
-        text-align:center;
-        background:#cccccc44;
-        color:#000000dd;
-        padding: 16px 0;
-        font-weight:700;
-        
-        
-            text-shadow: 
-        -1px -1px 0px #fff,
-         0px -1px 0px #fff,
-         1px -1px 0px #fff,
-        -1px  0px 0px #fff,
-         1px  0px 0px #fff,
-        -1px  1px 0px #fff,
-         0px  1px 0px #fff,
-         1px  1px 0px #fff;
-        
-    }
-`
+  :hover {
+    transform: scale(1.09);
+  }
+
+  span {
+    text-align: center;
+    background: #cccccc44;
+    color: #000000dd;
+    padding: 16px 0;
+    font-weight: 700;
+
+    text-shadow: -1px -1px 0px #fff, 0px -1px 0px #fff, 1px -1px 0px #fff,
+      -1px 0px 0px #fff, 1px 0px 0px #fff, -1px 1px 0px #fff, 0px 1px 0px #fff,
+      1px 1px 0px #fff;
+  }
+`;
