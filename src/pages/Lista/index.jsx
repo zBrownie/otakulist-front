c88df714bf8878bfbd08affc06361208e5989076
zBrownie <bru.nomoraes@hotmail.com>
2020-03-11 13:25:12 -0300

    update 11/03

--- src/pages/Lista/index.jsx
+++ src/pages/Lista/index.jsx
@@ -2,18 +2,20 @@ import React, { useState, useEffect } from "react";
 import api from "../../service/api";
 import { Container } from "./styles";
 import AnimesShow from "../../components/AnimesShow";
+import { useSelector } from "react-redux";
 
 export default function Lista() {
-  const [animes, setanimes] = useState([]);
-  useEffect(() => {
-    async function handleAllAnimes() {
-      await api.get("/animes").then(response => {
-        setanimes(response.data);
-      });
-    }
-    handleAllAnimes()
-  }, []);
+  // const [animes, setanimes] = useState([]);
+  // useEffect(() => {
+  //   async function handleAllAnimes() {
+  //     await api.get("/animes").then(response => {
+  //       setanimes(response.data);
+  //     });
+  //   }
+  //   handleAllAnimes()
+  // }, []);
 
+  const animes = useSelector(state => state.animes);
   return (
     <Container>
       <AnimesShow data={animes} title={""} />
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,27 +5,27 @@ import { Form } from "@unform/web";
 import Input from "../../components/Form/input";
 import { handleGetUser, handleGetToken } from "../../redux/Actions";
 import api from "../../service/api";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 export default function Login() {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleSubmit = async data => {
     await api
       .post("/auth", data)
       .then(response => {
-        dispatch(handleGetUser(response.data.user))
-        dispatch(handleGetToken(response.data.token))
+        dispatch(handleGetUser(response.data.user));
+        dispatch(handleGetToken(response.data.token));
       })
       .catch(error => {
         console.log("ERRO LOGIN", error);
       });
-       };
+  };
 
   return (
     <Container>
       <Form onSubmit={handleSubmit}>
+        <h1>Login</h1>
         <Input name="email" type="email" />
         <Input name="password" type="password" />
         <button type="submit">Entrar</button>
